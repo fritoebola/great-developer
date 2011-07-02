e9ae92077281a8c49a2a6ab9c52c43b7c22c1770
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2011-07-03 00:49:01 +0100

    Worked on battery slide

--- js/main.js
+++ js/main.js
@@ -14,7 +14,21 @@ function GoodDeveloperApp() {
 
     this.heartTaps = 0;
 
+    this.batteryDrag = false;
+    this.batteryStartX = parseInt( $('#battery').css('left').replace('px','') );
+    this.batteryStartY = parseInt( $('#battery').css('top').replace('px','') );
+    this.batteryContainerX = parseInt( $('#batteryContainer').css('left').replace('px','') );
+    this.batteryContainerY = parseInt( $('#batteryContainer').css('top').replace('px','') );
+
     this.prepareSwipe = function(event) {
+        
+        if( event.target.id == 'battery' ) {
+            // If battery, prepare drag and drop
+            batteryDrag = true;
+        } else {
+            // Otherwise it may change slides
+            batteryDrag = false;
+        }
         
         var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
 
@@ -56,28 +70,57 @@ function GoodDeveloperApp() {
 
     this.swipeRight = function(event) {
 
+        // Currently disabled
+        /*
         if( this.currentSlide > 1 ) {
             this.moveSlidesRight();
         }
+        */
 
     };
 
     this.handleSwipe = function(event) {
 
         var touch = event.originalEvent.touches[0] || event.originalEvent.changedTouches[0];
-        
-        var xDiff = touch.pageX - this.touchStartX;
-        var yDiff = touch.pageY - this.touchStartY;
 
-        var xMoveGreater = (Math.abs(xDiff) - Math.abs(yDiff) > 0);
+        if( batteryDrag ) {
 
-        // Assume swipe if x diff > 20 and x diff is more than y diff
-        if( xDiff < -20 && xMoveGreater ) {
-            this.swipeLeft();
-        } else if( xDiff > 20 && xMoveGreater ) {
-            this.swipeRight();
+            var newX = touch.pageX - this.batteryStartX - ($('#battery').width()/2);
+            var newY = touch.pageY - this.batteryStartY - ($('#battery').height()/2);
+
+            $('#battery').css('-webkit-transform', 'translate('+newX+'px,'+newY+'px)');
+
+            console.log( touch.pageX + ' >= ' + this.batteryContainerX);
+            console.log( touch.pageX + ' <= ' + (this.batteryContainerX + $('#batteryContainer').width()) );
+
+            console.log( touch.pageY + ' >= ' + this.batteryContainerY);
+            console.log( touch.pageY + ' <= ' + (this.batteryContainerY + $('#batteryContainer').height()) );
+
+            if( touch.pageX >= this.batteryContainerX && 
+                touch.pageX <= this.batteryContainerX + $('#batteryContainer').width() && 
+                touch.pageY >= this.batteryContainerY && 
+                touch.pageY <= this.batteryContainerY + $('#batteryContainer').height() ) {
+
+                this.moveSlidesLeft();
+
+            }
+
+        } else {
+            
+            var xDiff = touch.pageX - this.touchStartX;
+            var yDiff = touch.pageY - this.touchStartY;
+
+            var xMoveGreater = (Math.abs(xDiff) - Math.abs(yDiff) > 0);
+            
+            // Assume swipe if x diff > 20 and x diff is more than y diff
+            if( xDiff < -20 && xMoveGreater ) {
+                this.swipeLeft();
+            } else if( xDiff > 20 && xMoveGreater ) {
+                this.swipeRight();
+            }
+
         }
-
+        
     };
 
     this.increaseHeartSize = function() {
@@ -120,15 +163,21 @@ function GoodDeveloperApp() {
     this.setupTapEvents = function() {
 
         $('#heart').bind('touchend', function(e) { _this.doHeartBeat(); });
-        // Also allow clicks for debugging
+
+    };
+
+    // We may as well support as much on a non-touch device as we can
+    this.setupClickEvents = function() {
+        
         $('#heart').click(function(e) { _this.doHeartBeat(); });
         
-    }
+    };
 
     this.initialise = function() {
 
         this.setupSwipeEvents();
         this.setupTapEvents();
+        this.setupClickEvents();
 
     };
 
